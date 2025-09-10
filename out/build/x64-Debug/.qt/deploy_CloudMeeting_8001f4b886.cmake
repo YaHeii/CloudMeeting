530e20cd0293149c21105d7b6f8137dc1da30d16
呀嘿 <c15146681278@163.com>
2025-09-10 22:29:38 +0800

    从版本库中移除编译产物和IDE配置文件

--- out/build/x64-Debug/.qt/deploy_CloudMeeting_8001f4b886.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/code/C++/QT/CloudMeeting/out/build/x64-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/CloudMeeting-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE D:/code/C++/QT/CloudMeeting/out/build/x64-Debug/CloudMeeting.exe
-    GENERATE_QT_CONF
-)
